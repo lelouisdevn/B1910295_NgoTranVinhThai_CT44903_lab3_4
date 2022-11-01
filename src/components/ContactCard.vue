b10ecbdfcdbc53bbbd696c3971d7adb9257ecf77
lelouisdevn <atlanteansvietnam@outlook.com>
2022-11-02 04:55:06 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    props: {
+        contact: {type: Object, required: true },
+    },
+};
+</script>
+
+<template>
+    <div>
+        <div class="p-1">
+            <strong>Ten:</strong>
+            {{ contact.name }}
+        </div>
+        <div class="p-1">
+            <strong>Email:</strong>
+            {{ contact.email }}
+        </div>
+        <div class="p-1">
+            <strong>Dia chi:</strong>
+            {{ contact.address }}
+        </div>
+        <div class="p-1">
+            <strong>Dien thoai:</strong>
+            {{ contact.phone }}
+        </div>
+        <div class="p-1">
+            <strong>Lien he yeu thich:&nbsp;</strong>
+            <i v-if="contact.favorite" class="fas fa-check"></i>
+            <i class="fas fa-times" v-else></i>
+        </div>
+    </div>
+</template>
---
+++ src/components/ContactList.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    props: {
+        contactss: {type: Array, default: []},
+        activeIndex: {type: Number, default: -1},
+    },
+    emits: ["update:activeIndex"],
+    methods: {
+        updateActiveIndex(index) {
+            console.log(index);
+            this.$emit("update:activeIndex", index);
+        }
+    }
+};
+</script>
+
+<template>
+    <ul class="list-group">
+        <li 
+            class="list-group-item"
+            v-for="(contact, index) in contactss"
+            :key="contact._id"
+            :class="{ active: index === activeIndex }"
+            @click="updateActiveIndex(index)"
+        >
+            {{ contact.name }}
+        </li>
+    </ul>
+</template>
